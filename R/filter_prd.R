664558ee18b40a237711b1322d3dd48886326d85
svmiller <steven.v.miller@gmail.com>
2020-12-15 14:51:47 -0500

    change that

--- R/filter_prd.R
+++ R/filter_prd.R
@@ -37,9 +37,6 @@
 filter_prd <- function(data) {
   require(dplyr)
   require(magrittr)
-  require(dplyr)
-  require(tidyr)
-  require(stringr)
 
   if (length(attributes(data)$ps_data_type) > 0 && attributes(data)$ps_data_type == "dyad_year") {
 
