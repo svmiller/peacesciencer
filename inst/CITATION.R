d4f901321bfdb75d645f4311d544c6257267cf1a
svmiller <steven.v.miller@gmail.com>
2021-06-12 10:10:05 -0400

    addy mcaddadd

---
+++ inst/CITATION.R
@@ -0,0 +1,14 @@
+itHeader("Please cite {peacesciencer} in publications use. It helps out my meager h-index. Please cite as:")
+
+citEntry(entry = "Manual",
+         title        = "{peacesciencer}: A User's Guide for Quantitative Peace Science in R",
+         author       = person("Steven V. Miller"),
+         year         = "2021",
+         key          = "peacesciencer-package",
+         url          = "http://svmiller.com/peacesciencer",
+
+         textVersion  =
+           paste("Miller, Steven V. 2021.",
+                 "{peacesciencer}: A User's Guide for Quantitative Peace Science in R",
+                 "http://svmiller.com/peacesciencer")
+)
