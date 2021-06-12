2254854080cb92f885f8ab450a2e2bac2fd29400
svmiller <steven.v.miller@gmail.com>
2021-06-12 11:47:15 -0400

    CSS updates. Someone please hire me.

--- user-guide/src/render_html.R
+++ user-guide/src/render_html.R
@@ -1,4 +1,7 @@
 rmarkdown::render("ms.Rmd",
                   output_file="../docs/ms.html",
+                  params=list(ps_website = "http://svmiller.com/peacesciencer",
+                              ps_manuals = "http://svmiller.com/peacesciencer/reference",
+                              ps_github = "https://github.com/svmiller/peacesciencer/"),
                   bookdown::html_document2(template = stevetemplates::templ_html_article(),
                                            toc = FALSE, number_sections = FALSE))
