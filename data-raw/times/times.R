983abaf6018d0ad82164ced814797138cedbe36d
svmiller <steven.v.miller@gmail.com>
2021-06-13 12:35:49 -0400

    update

--- data-raw/times/times.R
+++ data-raw/times/times.R
@@ -5,8 +5,10 @@ library(peacesciencer)
 library(tictoc)
 tic.clearlog()
 
+# Create Times list ----
 Times <- list()
 
+# * create_statedays() -----
 for (x in 1:100) {
   # passing x to tic() makes it a label at time of the matching toc() call.
   tic(x)
@@ -16,9 +18,9 @@ for (x in 1:100) {
 }
 
 Times$"create_statedays()" <- tibble(x = unlist(lapply(tic.log(format = FALSE), function(x) x$toc - x$tic)))
-
-
 tic.clearlog()
+
+# * create_stateyears() -----
 
 for (x in 1:100) {
   tic(x)
@@ -28,9 +30,9 @@ for (x in 1:100) {
 }
 
 Times$"create_stateyears()" <- tibble(x = unlist(lapply(tic.log(format = FALSE), function(x) x$toc - x$tic)))
-
-
 tic.clearlog()
+
+# * create_dyadyears() -----
 
 for (x in 1:100) {
   tic(x)
@@ -40,5 +42,64 @@ for (x in 1:100) {
 }
 
 Times$"create_dyadyears()" <- tibble(x = unlist(lapply(tic.log(format = FALSE), function(x) x$toc - x$tic)))
+tic.clearlog()
+
+# * add_gml_mids(keep=NULL) -----
+
+for (x in 1:100) {
+  tic(x)
+  cow_ddy %>% add_gml_mids(keep = NULL)
+  gc()
+  toc(log = TRUE, quiet = TRUE)
+}
+
+Times$"add_gml_mids(keep=NULL)" <- tibble(x = unlist(lapply(tic.log(format = FALSE), function(x) x$toc - x$tic)))
+tic.clearlog()
+
+# * add_peace_years() (dyadic, GML) -----
+
+cow_ddy %>% add_gml_mids(keep = NULL) -> Data
+
+for (x in 1:100) {
+  tic(x)
+  Data %>% add_peace_years()
+  gc()
+  toc(log = TRUE, quiet = TRUE)
+}
+
+Times$"add_peace_years() (dyadic, GML)" <- tibble(x = unlist(lapply(tic.log(format = FALSE), function(x) x$toc - x$tic)))
+tic.clearlog()
+
+# * add_peace_years() (state, GW) -----
+
+rm(Data)
+
+create_stateyears(system='gw') %>% add_ucdp_acd() -> Data
+
+for (x in 1:100) {
+  tic(x)
+  Data %>% add_peace_years()
+  gc()
+  toc(log = TRUE, quiet = TRUE)
+}
+
+Times$"add_peace_years() (state, GW)" <- tibble(x = unlist(lapply(tic.log(format = FALSE), function(x) x$toc - x$tic)))
+tic.clearlog()
+
+# * add_peace_years() (state, CoW [intra]) -----
+
+rm(Data)
+
+create_stateyears(system='cow') %>% add_cow_wars(type='intra') -> Data
+
+for (x in 1:100) {
+  tic(x)
+  Data %>% add_peace_years()
+  gc()
+  toc(log = TRUE, quiet = TRUE)
+}
+
+Times$"add_peace_years() (state, CoW [intra])" <- tibble(x = unlist(lapply(tic.log(format = FALSE), function(x) x$toc - x$tic)))
+tic.clearlog()
 
 saveRDS(Times, "~/Dropbox/projects/peacesciencer/data-raw/times/Times.rds")
