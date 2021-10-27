6263222225576c14e60f658601b3d4478ed1a838
svmiller <steven.v.miller@gmail.com>
2021-10-27 14:15:54 -0400

    sfsfsfs

--- R/create_leaderdyadyears.R
+++ R/create_leaderdyadyears.R
@@ -9,7 +9,7 @@
 #'
 #' @details This is a *complete* and *universal* leader dyad-year data frame for all possible dyadic
 #' leader pairings from 1870 to 2015. This has several implications. First: these data are enormous. The
-#' output is over 2.4 million rows long! Second: the time required to create these data from scratch would
+#' output is over 2 million rows long! Second: the time required to create these data from scratch would
 #' take too long for a normal function call. This amounts to an unholy combination of data that are too large
 #' for CRAN's disk space restrictions (5 MB) and too time-consuming to do from scratch every time. Thus, the
 #' data are pre-generated and stored remotely. Check \code{download_extdata()} for more information.
@@ -24,6 +24,9 @@
 #' \emph{Journal of Peace Research} 46(2): 269--83.
 #'
 #'
+#' @param system a character specifying whether the user wants Correlates of War
+#' state-years ("cow") or Gleditsch-Ward ("gw") state-years. Gleditsch-Ward is the
+#' default.
 #' @param directed optional, defaults to TRUE. If TRUE, the function returns so-called
 #' "directed" leader dyad-year data. If FALSE, the function returns
 #' non-directed data. The standard here is to drop cases where the country code for the
@@ -40,28 +43,54 @@
 #' create_leaderdyadyears(directed = FALSE)
 #' }
 #'
-create_leaderdyadyears <- function(directed = TRUE) {
+create_leaderdyadyears <- function(directed = TRUE, system = "gw") {
 
-  if (!file.exists(system.file("extdata", "dir_leader_dyad_years.rds", package="peacesciencer"))) {
+  if (!(system %in% c("cow", "gw"))) {
+    stop("create_leaderdyadyears() requires the system argument to be either 'cow' (Correlates of War) or 'gw' (Gleditsch-Ward). Default is 'gw'.")
+
+  }
+
+  if (!file.exists(system.file("extdata", "gw_dir_leader_dyad_years.rds", package="peacesciencer"))) {
 
     stop("The process of creating these data would take too long for a normal function. Thus, they are pre-generated, stored remotely, and must be downloaded separately.\nThis error disappears after successfully running `download_extdata()`. Thereafter, the function works with no problem.")
 
   } else {
 
-    readRDS(system.file("extdata", "dir_leader_dyad_years.rds", package="peacesciencer")) %>%
-      select(-.data$startdate1, -.data$enddate1, -.data$startdate2, -.data$enddate2) -> data
+    if (system == "gw") {
 
-    if (directed == FALSE) {
+      readRDS(system.file("extdata", "gw_dir_leader_dyad_years.rds", package="peacesciencer")) -> data
 
-      data %>%
-        filter(.data$ccode1 < .data$ccode2) -> data
+      if (directed == FALSE) {
+
+        data %>%
+          filter(.data$gwcode1 < .data$gwcode2) -> data
+
+      }
+    } else { # Assuming it's CoW.
+
+      readRDS(system.file("extdata", "cow_dir_leader_dyad_years.rds", package="peacesciencer")) -> data
+
+      if (directed == FALSE) {
+
+        data %>%
+          filter(.data$ccode1 < .data$ccode2) -> data
+
+      }
 
     }
+
+
+
+
 
   }
 
   attr(data, "ps_data_type") = "dyad_year"
-  attr(data, "ps_system") = "cow"
+  if (system == "cow") {
+    attr(data, "ps_system") = "cow"
+  } else {
+    attr(data, "ps_system") = "gw"
+  }
 
   return(data)
 
