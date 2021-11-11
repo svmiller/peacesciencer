fb48a61264a69f342667819bc8906077383ab58a
svmiller <steven.v.miller@gmail.com>
2021-11-11 08:16:07 -0500

    another day, another update

--- user-guide/R/1-create-gw_statedays.R
+++
@@ -1,6 +0,0 @@
-library(tidyverse)
-library(peacesciencer)
-
-create_statedays(system = 'gw') -> gw_statedays
-
-saveRDS(gw_statedays, "data/gw_statedays.rds")
--- user-guide/R/2-analysis.R
+++
@@ -1,8 +0,0 @@
-Data <- readRDS("user-guide/data/Data.rds")
-
-Mods <- list()
-
-Mods$"Default" <- lm(immigsent ~ agea + female + eduyrs + uempla + hinctnta + lrscale, data=Data)
-Mods$"Default + Noise" <- lm(immigsent ~ agea + female + eduyrs + uempla + hinctnta + lrscale + noise, data=Data)
-
-saveRDS(Mods, "user-guide/data/Mods.rds")
